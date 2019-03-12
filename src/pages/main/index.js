586e579346c831820ba66c420e2fa00d6749f672
Thais Moraes <murici.thais@gmail.com>
2019-03-12 12:31:55 -0300

    Adding basic structure of main page

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,5 +1,35 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 
-const Main = () => <h1>Main</h1>;
+export default class Main extends Component {
+  state = {
+    repositoryInput: '',
+  };
 
-export default Main;
+  render() {
+    const { repositoryInput } = this.state;
+    return (
+      <Fragment>
+        <form onSubmit={this.handleAddRepository} action="">
+          <input
+            type="text"
+            placeholder="user/repository"
+            value={repositoryInput}
+            onChange={e => this.setState({ repositoryInput: e.target.value })}
+          />
+          <button type="submit">Add</button>
+        </form>
+
+        <ul>
+          <li>
+            <p>
+              <strong>facebook/react</strong>
+              {''}
+              (description)
+            </p>
+            <a href="http://github.com/facebook/react">See repo</a>
+          </li>
+        </ul>
+      </Fragment>
+    );
+  }
+}
