24668630d02a92556aacd660717348ff69d4ac63
Thais Moraes <murici.thais@gmail.com>
2019-03-12 19:22:15 -0300

    Fixing Saga connection to Reactotron

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,27 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import Reactotron from 'reactotron-react-js';
 
 import reducers from './reducers';
 import sagas from './sagas';
 
 const middlewares = [];
 
-const sagaMiddleware = createSagaMiddleware();
+const sagaMonitor = Reactotron.createSagaMonitor();
+
+const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
 
 middlewares.push(sagaMiddleware);
 
 const tronMiddleware = process.env.NODE_ENV === 'development' ? console.tron.createEnhancer : () => {};
 
-const store = createStore(reducers, compose(tronMiddleware(), applyMiddleware(...middlewares)));
+const store = createStore(
+  reducers,
+  compose(
+    tronMiddleware(),
+    applyMiddleware(...middlewares),
+  ),
+);
 
 sagaMiddleware.run(sagas);
 
