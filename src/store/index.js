67a12289f7decfbe71c25ed34783d2973c7f5c2c
Thais Moraes <murici.thais@gmail.com>
2019-03-12 20:15:54 -0300

    Checking env type to create saga monitor

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,12 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
-import Reactotron from 'reactotron-react-js';
 
 import reducers from './reducers';
 import sagas from './sagas';
 
 const middlewares = [];
 
-const sagaMonitor = Reactotron.createSagaMonitor();
+const sagaMonitor = process.env.NODE_ENV === 'development' ? console.tron.createSagaMonitor() : null;
 
 const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
 
